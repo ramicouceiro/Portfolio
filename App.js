250d834e0f8fa0173626de0e50210893d10dcf04
ramicouceiro <coucerama@gmail.com>
2023-01-25 19:06:51 -0300

    slider proyectos terminado

--- App.js
+++ App.js
@@ -10,4 +10,56 @@ $(document).ready(function () {
   menuLink.click(function () {
     menu.toggleClass("nav_open");
   });
+
+  const project = $(".project");
+
+  const renderProjects = (p) => {
+    project.html(`
+    <div class="project__img">
+      <img src=${p.img} alt="" />
+    </div>
+    <div class="project__content">
+      <h3>${p.project}</h3>
+      <p>
+        ${p.descripcion}
+      </p>
+      <div class="project__languages">
+        <span class="project__language">HTML</span>
+        <span class="project__language">CSS</span>
+        <span class="project__language">JavaScript</span>
+        <span class="project__language">Responsive</span>
+      </div>
+      <a
+        href="${p.link}"
+        target="_blank"
+        class="hero__btn"
+        >Ver Proyecto</a>
+    </div>
+`);
+  };
+
+  renderProjects(projectData[0]);
+
+  // Boton siguiente y anterior
+
+  const nextBtn = $(".fa-angle-right");
+  const prevBtn = $(".fa-angle-left");
+
+  let index = 0;
+
+  nextBtn.click(function () {
+    index++;
+    if (index > projectData.length - 1) {
+      index = 0;
+    }
+    renderProjects(projectData[index]);
+  });
+
+  prevBtn.click(function () {
+    index--;
+    if (index < 0) {
+      index = projectData.length - 1;
+    }
+    renderProjects(projectData[index]);
+  });
 });
